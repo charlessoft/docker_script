fb392873b96899b1b4904b896c7b20d659e052de
charlessoft <qian01_chen@foxitsoftware.cn>
2020-01-02 11:00:59 +0800

    Changed: mysql 配置文件注释

--- mysql/config.sh
+++ mysql/config.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
+# ===== mysql 配置=====#
+
+# 根路径
 MYSQL_ROOT=${PWD}
 
+# mysql 镜像
 MYSQLIMAGE=mysql:5.7.23
-PORT=3306
-DOCKER_MYSQL_BACKUP_FOLDER=${MYSQL_ROOT}/backups
 
-# data folder
-DOCKER_MYSQL_VOLUME=${MYSQL_ROOT}/mysql/data
-
-# mysql config
-
-DOCKER_MYSQL_CONFIG=${MYSQL_ROOT}/conf
-
-# 数据库密码,
+# 数据库密码
 ROOT_PASSWORD=root
 
+# 容器名字
 CONTAINER_NAME=mysql
 
+# 对外端口
+PORT=3306
 
+# 数据库备份路径
+DOCKER_MYSQL_BACKUP_FOLDER=${MYSQL_ROOT}/backups
+
+# 数据库挂载路径
+DOCKER_MYSQL_VOLUME=${MYSQL_ROOT}/mysql/data
+
+# mysql 配置信息
+DOCKER_MYSQL_CONFIG=${MYSQL_ROOT}/conf
 
 # for backup.sh
 # 1. 需要制定导出的备份的数据库名字.
 BACK_DATABSE_NAME=xxxxx
+
+# ===== mysql 配置=====#
