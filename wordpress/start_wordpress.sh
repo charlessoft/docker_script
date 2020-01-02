1db4a8fcc9ea5fb786c6b5db424731a82a62bdea
charlessoft <qian01_chen@foxitsoftware.cn>
2020-01-02 11:20:48 +0800

    Changed: wordpress 配置

--- wordpress/start_wordpress.sh
+++ wordpress/start_wordpress.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 source ./wordpress_config.sh
 
-docker run --restart=always -d --name ${WORDPRESS_CONTAINER} \
-	--env WORDPRESS_DB_PASSWORD=${WORDPRESS_DB_PASSWORD}\
-    -p 8080:80 \
-    -v ${PWD}/wp-app:/var/www/html \
-    -v ${PWD}/theme-name/dux/:/var/www/html/wp-content/themes/dux \
-    -v ${PWD}/config/php.conf.ini:/usr/local/etc/php/conf.d/conf.ini \
-    --link ${SQLDB_CONTAINER}:mysql \
-    ${WORDPRESS_IMAGE}
+# link
+# docker run --restart=always -d --name ${WORDPRESS_CONTAINER} \
+# 	--env WORDPRESS_DB_PASSWORD=${WORDPRESS_DB_PASSWORD}\
+#     -p ${WORDPRESS_PORT}:80 \
+#     -v ${PWD}/wp-app:/var/www/html \
+#     -v ${PWD}/theme-name/dux/:/var/www/html/wp-content/themes/dux \
+#     -v ${PWD}/conf/php.conf.ini:/usr/local/etc/php/conf.d/conf.ini \
+#     --link ${SQLDB_CONTAINER}:mysql \
+#     ${WORDPRESS_IMAGE}
+
+
+
+docker run --restart=always -d \
+    --name ${WORDPRESS_CONTAINER} \
+    -e WORDPRESS_DB_HOST=${WORDPRESS_DB_HOST} \
+    -e WORDPRESS_DB_PASSWORD=${WORDPRESS_DB_PASSWORD} \
+    -e WORDPRESS_DB_USER=${WORDPRESS_DB_USER} \
+    -e WORDPRESS_DB_NAME=${WORDPRESS_DB_NAME} \
+     -p ${WORDPRESS_PORT}:80 \
+     -v ${PWD}/wp-app:/var/www/html \
+     -v ${PWD}/theme-name/dux/:/var/www/html/wp-content/themes/dux \
+     -v ${PWD}/conf/php.conf.ini:/usr/local/etc/php/conf.d/conf.ini \
+     ${WORDPRESS_IMAGE}
+
+# ===== wordpress 配置 =====#
--- wordpress/wordpress_config.sh
+++ wordpress/wordpress_config.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
+
+# ===== wordpress 配置 =====#
+
+# wordpress 镜像
+WORDPRESS_IMAGE=wordpress
+
+# wordpress 容器名
+WORDPRESS_CONTAINER=wordpress
+
+# wordpress 对外服务端口
+WORDPRESS_PORT=8080
+
+
+# 数据库容器名字, 单机部署做link用
+# SQLDB_CONTAINER=mysql
+
+# 数据库ip
+WORDPRESS_DB_HOST=127.0.0.1:3306
+
 # 数据库root 密码
 WORDPRESS_DB_PASSWORD=root
 
+# 用户名
+WORDPRESS_DB_USER=root
 
-# 数据库容器名字, 做link用
-SQLDB_CONTAINER=mysql
+# wordpress数据库名字
+WORDPRESS_DB_NAME=wordpress
 
-WORDPRESS_IMAGE=wordpress
-
-
-WORDPRESS_CONTAINER=wordpress
+# ===== wordpress 配置 =====#
