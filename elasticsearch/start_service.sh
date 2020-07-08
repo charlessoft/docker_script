dd5c74ade425938d448b45c5de0c35be1ef225eb
qian_chen <qian01_chen@foxitsoftware.cn>
2020-07-08 20:11:53 +0800

    add esbak

--- elasticsearch/start_service.sh
+++ elasticsearch/start_service.sh
@@ -5,6 +5,7 @@ docker run \
     -v ${ES_DATA}:/usr/share/elasticsearch/data \
     -v ${ES_CONFIG}:/usr/share/elasticsearch/config \
     -v ${ES_PLUGINS}:/usr/share/elasticsearch/plugins \
+    -v ${ES_BACKUP}:/tmp/es_backup \
     -e TAKE_FILE_OWNERSHIP=111 \
     --name ${CONTAINER_NAME} -d ${ESIMAGE}
 
