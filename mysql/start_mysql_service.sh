7241b8dffcbdc02bf6d09494bd7d5f52ae057cd8
charlessoft <charlessoft@qq.com>
2020-07-12 18:41:41 +0800

    update start

--- mysql/start_mysql_service.sh
+++ mysql/start_mysql_service.sh
@@ -5,5 +5,7 @@ source ./config.sh
 docker run -p ${PORT}:3306 \
     --name ${CONTAINER_NAME} \
     -v ${DOCKER_MYSQL_CONFIG}:/etc/mysql/conf.d \
-    -v ${PWD}/mysql/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=${ROOT_PASSWORD}  -d ${MYSQLIMAGE}
+    -v ${PWD}/mysql/data:/var/lib/mysql \
+    -e MYSQL_ROOT_PASSWORD=${ROOT_PASSWORD} \
+    -d ${MYSQLIMAGE}
 
