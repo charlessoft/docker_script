5687be851b7c2fb1cb4978e7dd87a0d518efc829
qian_chen <qian01_chen@foxitsoftware.cn>
2020-09-03 14:45:09 +0800

    add kibana

---
+++ kibana/start_service.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# docker pull docker.elastic.co/kibana/kibana:7.3.2
+
+# docker run -it -d -e \
+#     ELASTICSEARCH_URL=${ELASTICSEARCH_URL} \
+#     --name kibana \
+#     -p ${KIBANA_PORT}:5601 \
+#     docker.elastic.co/kibana/kibana:7.3.2
+
+
+docker run --name kibana -p 5601:5601 -v ${PWD}/kibana.yml:/usr/share/kibana/config/kibana.yml kibana:7.3.2
