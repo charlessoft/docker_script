e06a9242cb0dc84198551e98219380e0ac70b8b5
qian_chen <qian01_chen@foxitsoftware.cn>
2020-09-08 19:26:49 +0800

    add backgroud kibana

--- kibana/start_service.sh
+++ kibana/start_service.sh
@@ -8,4 +8,4 @@
 #     docker.elastic.co/kibana/kibana:7.3.2
 
 
-docker run --name kibana -p 5601:5601 -v ${PWD}/kibana.yml:/usr/share/kibana/config/kibana.yml kibana:7.3.2
+docker run -d --name kibana -p 5601:5601 -v ${PWD}/kibana.yml:/usr/share/kibana/config/kibana.yml kibana:7.3.2
