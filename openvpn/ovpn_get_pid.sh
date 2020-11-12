f99cdd1a6565c184e9c26bc7ff5037d673c42d67
charlessoft <charlessoft@qq.com>
2020-11-12 21:11:51 +0800

    update

--- openvpn/ovpn_get_pid.sh
+++ openvpn/ovpn_get_pid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT=`basename $0`
 export CURPWD=$(cd `dirname $0`; pwd)
-pid=`ps -ef|grep openvpn| grep -v "grep" | grep -v "ovpn_get_pid"`
+pid=`ps -ef|grep openvpn| grep -v "grep" | grep -v "ovpn_get_pid" | grep -v "ovpn_get_running_time"`
 if [ $? -eq 0 ]; then
     echo $pid |awk '{print $2}'|sed -n '1p' > ${CURPWD}/pid.txt
 else
