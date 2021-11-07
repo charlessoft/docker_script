527b766f524cb3cc85e035e5148b33ada9632196
charlessoft <charlessoft@qq.com>
2021-11-07 21:46:32 +0800

    Fixed: 调整删除用户

--- samba/delete.sh
+++ samba/delete.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 username=$1
+config_name=`echo "$1" | tr 'A-Z' 'a-z'`
+config_path=$config_name'.smb.conf'
+
 pdbedit -x -u $username
 
 userdel -r $username
 
-rm -fr "/etc/samba/config/${username}.smb.conf"
+rm -fr "/etc/samba/config/${config_path}"
