07f8b33c452e3e114cb24e9dbc95891e32137c12
qian01_chen <qian01_chen@foxitsoftware.com>
2021-11-08 10:03:53 +0800

    Changed: docker 版本:

--- docker/config.sh
+++ docker/config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-DOCKER_PACKAGE_URL=https://download.docker.com/linux/static/stable/x86_64/docker-18.06.3-ce.tgz
+DOCKER_PACKAGE_URL=https://download.docker.com/linux/static/stable/x86_64/docker-19.03.14.tgz
 
 # https://github.com/docker/compose/releases/download/1.21.2/docker-compose-Linux-x86_64
-DOCKER_COMPOSE_URL="https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m)"
+DOCKER_COMPOSE_URL="https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)"
 
 export DOCKER_PACKAGE=${DOCKER_PACKAGE_URL##*/}
 export DOCKER_COMPOSE=${DOCKER_COMPOSE_URL##*/}
