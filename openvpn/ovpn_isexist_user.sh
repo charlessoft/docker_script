78453b35241a7e689408a4826485c2281831b875
qian01_chen <qian01_chen@foxitsoftware.com>
2022-08-18 19:16:21 +0800

    Changed: add isexist user

---
+++ openvpn/ovpn_isexist_user.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+OVPN_USER_KEYS_DIR=/etc/openvpn/client/keys
+EASY_RSA_VERSION=easyrsa3
+EASY_RSA_DIR=/root/openvpn/softs/easy-rsa
+
+user=$1
+echo $user
+
+if [ "$user" = "" ]
+then
+    echo "user is not set!"
+    exit 1
+else
+    ls /etc/openvpn/client/keys/$user
+    # ls /tmp/keys/$user
+    #echo "/tmp/keys/$user"
+
+    if [ $? -ne 0  ]; then
+        echo "${user} is not found"
+        exit 1
+    else
+        echo "success"
+        exit 0
+    fi
+
+fi
+
+
+exit 0
+
